218db264b93a4429ab6c163ed967bfa22a2df867
Pranjal Nehete <pranjalnehete02@gmail.com>
2023-09-11 08:52:05 +0530

    Update2

---
+++ JavaApp/1.Intro/Arithmetic.java
@@ -0,0 +1,18 @@
+class Arithmetic{
+    public static void main(String[] args){
+        int sum, sub, mul, div;
+        int a = 4;
+        int b = 2;
+
+        sum = a + b;
+        sub = a - b;
+        mul = a * b;
+        div = a / b;
+
+        System.out.println("Lets a = 4 and b = 2, Then\n" );
+        System.out.println("The addition of a and b is " + sum);
+        System.out.println("The Substration of a and b is " + sub);
+        System.out.println("The Multiplication of a and b is " + mul);
+        System.out.println("The Division of a and b is " + div);
+    }
+}
